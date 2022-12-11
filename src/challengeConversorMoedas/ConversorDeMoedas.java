6896dfc580942d93144584247323a7247c2cc66d
hectorcamp <hectorcamp@gmail.com>
2022-12-10 23:29:57 -0300

    Primeiro_Commit

---
+++ src/challengeConversorMoedas/ConversorDeMoedas.java
@@ -0,0 +1,88 @@
+package challengeConversorMoedas;
+
+public class ConversorDeMoedas {
+        
+        public static double dolarParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaDolar(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double euroParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaEuro(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double libraParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaLibra(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double pesoParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaPeso(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double ieneParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaIene(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double bitcoinParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaBitcoin(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double ethereumParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaEthereum(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double litecoinParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaLitecoin(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double rippleParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaRipple(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double tetherParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+        
+        public static double realParaTether(double valor, double cotacao) {
+            return valor / cotacao;
+        }
+        
+        public static double cardanoParaReal(double valor, double cotacao) {
+            return valor * cotacao;
+        }
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hecto
+ *
+ */
+module challengeConversorMoedas {
+}
